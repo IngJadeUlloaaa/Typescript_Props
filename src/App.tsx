270bb06911f301faf3470d4bae50bc485c91d8fa
Jade <ulloacarlos3112@gmail.com>
2024-11-27 22:04:27 -0600

    first commit to star app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <main className='w-full h-svh bg-gray-900'>
+        <h1>hello world</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
